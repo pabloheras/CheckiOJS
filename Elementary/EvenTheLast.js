fb33bf2d2605f6109f9516a66ccfb967abbacba3
Pablo <pabloherascampa@gmail.com>
2017-02-28 17:03:30 +0100

    Eighth task

---
+++ Elementary/EvenTheLast.js
@@ -0,0 +1,36 @@
+/*
+You are given an array of integers. You should find the sum of the elements with even indexes 
+(0th, 2nd, 4th...) then multiply this summed number and the final element of the array together. 
+
+For an empty array, the result will always be 0 (zero).
+*/
+
+"use strict";
+
+function evenLast(data) {
+
+    let i= 0; //iterator
+    let result = 0; //final result container
+    
+    //If the array is not empty
+    if(data.length){
+        while(i<data.length){
+            result = result + data[i];
+            i = i+2;
+        }
+        
+        result = result * data[data.length-1];
+    }     
+    
+    return result;
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(evenLast([0, 1, 2, 3, 4, 5]), 30, "(0+2+4)*5=30");
+    assert.equal(evenLast([1, 3, 5]), 30, "(1+5)*5=30");
+    assert.equal(evenLast([6]), 36, "(6)*6=36");
+    assert.equal(evenLast([]), 0, "An empty array = 0");
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
---
+++ Elementary/SecretMessage.js
@@ -0,0 +1,32 @@
+/*
+You are given a chunk of text. Gather all capital letters in one word in the order that they appear 
+in the text.
+
+For example: text = "How are you? Eh, ok. Low or Lower? Ohhh.", if we collect all of the capital letters,
+we get the message "HELLO".
+*/
+
+"use strict"
+
+function findMessage(data) {
+    
+    let i
+    let secretMessage = ""
+    
+    //Check capital letters (using Regular Expression) and concatenate them in secretMessage variable
+    for(i=0; i<data.length; i++){
+        if(/[A-Z]/.test(data[i]))
+            secretMessage = secretMessage + data[i]
+    }
+    
+    return secretMessage
+}
+
+let assert = require('assert')
+
+if (!global.is_checking) {
+    assert.equal(findMessage("How are you? Eh, ok. Low or Lower? Ohhh."), "HELLO", "hello")
+    assert.equal(findMessage("hello world!"), "", "Nothing")
+    assert.equal(findMessage("HELLO WORLD!!!"), "HELLOWORLD", "Capitals")
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+}
